72824d7c4542e0bc6f6a04d1270794954854683e
Vikas_Android_Engineer <vicay0001@gmail.com>
2023-02-26 22:42:18 +0530

    note add data

--- app/src/main/java/com/me/jetnoteapp/MainActivity.kt
+++ app/src/main/java/com/me/jetnoteapp/MainActivity.kt
@@ -6,10 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.me.jetnoteapp.screens.NoteScreen
 import com.me.jetnoteapp.ui.theme.JetNoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    NoteScreen(notes = emptyList(), onAddNote = {}, onRemoveNote = {})
                 }
             }
         }
--- app/src/main/java/com/me/jetnoteapp/screens/NoteScreen.kt
+++ app/src/main/java/com/me/jetnoteapp/screens/NoteScreen.kt
@@ -3,6 +3,8 @@ package com.me.jetnoteapp.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
@@ -16,10 +18,16 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.me.jetnoteapp.R
+import com.me.jetnoteapp.components.NoteButton
 import com.me.jetnoteapp.components.NoteInputText
+import com.me.jetnoteapp.model.Note
 
 @Composable
-fun NoteScreen(){
+fun NoteScreen(
+    notes : List<Note>,
+    onAddNote : (Note) -> Unit,
+    onRemoveNote : (Note) -> Unit){
+
     var title by remember {
         mutableStateOf("")
     }
@@ -29,26 +37,58 @@ fun NoteScreen(){
 
     Column(modifier = Modifier.padding(6.dp)) {
         TopAppBar(title = {
-                          Text(text = stringResource(R.string.app_name))
+            Text(text = stringResource(R.string.app_name))
         }, actions = {
-            Icon(imageVector = Icons.Rounded.Notifications,
-                contentDescription = "Icon")
+            Icon(
+                imageVector = Icons.Rounded.Notifications,
+                contentDescription = "Icon"
+            )
         },
-        backgroundColor = Color(0xFF3FD1DE)
+            backgroundColor = Color(0xFF3FD1DE)
         )
-        
-        Column(modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally) {
 
-          NoteInputText(
-              text = title,
-              label = "Title",
-              onTextChange = {} )
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-          NoteInputText(
-              text = description,
-              label = "Add a note",
-              onTextChange = {} )
+            NoteInputText(
+                modifier = Modifier.padding(
+                    top = 9.dp,
+                    bottom = 9.dp
+                ),
+                text = title,
+                label = "Title",
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace()
+                        }) title = it
+                })
+
+            NoteInputText(
+                modifier = Modifier.padding(
+                    top = 9.dp,
+                    bottom = 9.dp
+                ),
+                text = description,
+                label = "Add a note",
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace()
+                        }) description = it
+                })
+
+            NoteButton(text = "Save", onClick = {
+                if (title.isNotEmpty() && description.isNotEmpty()) {
+                    // save or add to the list
+                    title = ""
+                    description = ""
+                }
+            })
+        }
+        Divider(modifier = Modifier.padding(10.dp))
+        LazyColumn {
+
         }
     }
 }
@@ -56,5 +96,5 @@ fun NoteScreen(){
 @Preview(showBackground = true)
 @Composable
 fun NoteScreensPreview(){
-    NoteScreen()
+    NoteScreen(notes = emptyList(), onAddNote = {}, onRemoveNote = {})
 }
